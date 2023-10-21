e4a09dbd61b1c1ca6a944c2d7a21dddbf16d60ae
Midhun Balachandran <midhunbalachandran07@gmail.com>
2023-10-21 10:30:39 +0530

    The python password generator .Simple and tough password generator created

---
+++ tough password generator/tough_password_generator.py
@@ -0,0 +1,50 @@
+import random
+
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f' , 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v' , 'w', 'x', 'y', 'z' , 'A', 'B', 'C', 'D', 'E' , 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' , 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+symbols = ['!', '@', '#', '$', '%', '&', '*', '_', '+', '.', '/', '?']
+
+
+print("Welcome to the Password Generator")
+
+no_alphabets = int(input("How many alphabets to you want to generate a password ? \n"))
+
+no_numbers = int(input("How many numbers to you want to generate a password? \n"))
+
+no_symbols = int(input("How many symbols to you want to generate a password? \n"))
+
+password_list = []
+
+for i in range(1 , no_alphabets + 1):
+
+  char = random.choice(alphabets)
+
+  password_list += char
+
+for i in range(1 , no_numbers + 1):
+
+  num = random.choice(numbers)
+
+  password_list += num  
+
+for i in range(1 , no_symbols + 1):
+
+  sym = random.choice(symbols)
+
+  password_list += sym
+
+random.shuffle(password_list)
+
+password = ""
+
+for char in password_list:
+
+  password += char
+
+print(password)  
+
+
+
